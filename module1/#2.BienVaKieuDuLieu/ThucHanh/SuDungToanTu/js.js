163101fd65a81b0b5cc9717aeacedd0a657da195
= <=>
2020-02-26 09:39:19 +0700

    update

---
+++ module1/#2.BienVaKieuDuLieu/ThucHanh/SuDungToanTu/js.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeigth;
+inputWidth = prompt("nhập vào chiều rộng: ");
+inputHeigth = prompt("nhập vào chiều cao: ");
+let width = parseInt(inputWidth);
+let heigth = parseInt(inputHeigth);
+let area = width * heigth;
+document.write("the area is: " + area);
